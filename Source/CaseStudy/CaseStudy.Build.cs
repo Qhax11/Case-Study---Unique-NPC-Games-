f3e68742ab582c05b05c608c2ada0a2c92a5cf9d
Şamil <samilozel7@gmail.com>
2025-01-28 23:55:11 +0300

    Cleaning on folder structure

--- Source/CaseStudy/CaseStudy.Build.cs
+++ Source/CaseStudy/CaseStudy.Build.cs
@@ -8,6 +8,12 @@ public class CaseStudy : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", 
+			"CoreUObject", 
+			"Engine", 
+			"InputCore",
+			"HeadMountedDisplay", 
+			"EnhancedInput" 
+		});
 	}
 }
